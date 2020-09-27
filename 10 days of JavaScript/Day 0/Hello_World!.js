207cc4c4e861acb35fbb9414898ff4a4b76b31ea
Ritika Bansal <ritikabansal5678@gmail.com>
2020-09-27 17:53:56 +0530

    first commit

---
+++ 10 days of JavaScript/Day 0/Hello_World!.js
@@ -0,0 +1,11 @@
+/*
+Objective
+
+In this challenge, we review some basic concepts that will get you started with this series. Check out the tutorial to learn more about JavaScript's lexical structure.
+
+Task
+
+A greeting function is provided for you in the editor below. It has one parameter,parameterVariable. Perform the following tasks to complete this challenge:
+1. Use console.log() to print Hello, World! on a new line in the console, which is also known as stdout or standard output. The code for this portion of the task is already provided in the editor.
+2. Use console.log() to print the contents of  (i.e., the argument passed to main).
+*/
