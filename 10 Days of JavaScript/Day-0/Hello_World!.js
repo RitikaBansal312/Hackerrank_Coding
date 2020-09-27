05e4fbca83d2c409244f28113a01ee600014e7e9
Ritika Bansal <ritikabansal5678@gmail.com>
2020-09-27 18:27:58 +0530

    first commit

---
+++ 10 Days of JavaScript/Day-0/Hello_World!.js
@@ -0,0 +1,60 @@
+/*
+Objective
+
+In this challenge, we review some basic concepts that will get you started with this series. Check out the tutorial to learn more about JavaScript's lexical structure.
+
+Task
+
+A greeting function is provided for you in the editor below. It has one parameter,parameterVariable. Perform the following tasks to complete this challenge:
+1. Use console.log() to print Hello, World! on a new line in the console, which is also known as stdout or standard output. The code for this portion of the task is already provided in the editor.
+2. Use console.log() to print the contents of parameterVariable(i.e., the argument passed to main).
+*/
+
+
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+/**
+*   A line of code that prints "Hello, World!" on a new line is provided in the editor. 
+*   Write a second line of code that prints the contents of 'parameterVariable' on a new line.
+*
+*	Parameter:
+*   parameterVariable - A string of text.
+**/
+function greeting(parameterVariable) {
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');
+    console.log(parameterVariable);
+    // Write a line of code that prints parameterVariable to stdout using console.log:
+    
+}
+
+
+function main() {
+    const parameterVariable = readLine();
+    
+    greeting(parameterVariable);
+}
