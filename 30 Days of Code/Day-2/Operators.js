a4da1bc71c33d47129f778e4b36325472474182d
Ritika Bansal <ritikabansal5678@gmail.com>
2020-10-11 22:18:57 +0530

    first commit

---
+++ 30 Days of Code/Day-2/Operators.js
@@ -0,0 +1,8 @@
+/*
+Objective
+In this challenge, you'll work with arithmetic operators. Check out the Tutorial tab for learning materials and an instructional video!
+
+Task
+Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost.
+*/
+
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Hackerrank_Coding
-Solutions to HackerRank problems |  HackerRank's 10 Days of Javascript challenge
+Solutions to HackerRank problems |  HackerRank's 10 Days of Javascript challenge | 30 Days of Code
