feb7482f96b82b9f5860fce467430b7028fb5f24
Ritika Bansal <ritikabansal5678@gmail.com>
2020-10-11 22:22:01 +0530

    first commit

--- 10 Days of JavaScript/Day-0/Hello_World!.js
+++ 10 Days of JavaScript/Day-0/Hello_World!.js
@@ -46,8 +46,8 @@ function readLine() {
 function greeting(parameterVariable) {
     // This line prints 'Hello, World!' to the console:
     console.log('Hello, World!');
-    console.log(parameterVariable);
     // Write a line of code that prints parameterVariable to stdout using console.log:
+    console.log(parameterVariable);
     
 }
 
